040347f7880c120b94cab71b21abd9fff88c6bce
Byte Chakra <byte.chakra@gmail.com>
2022-10-09 23:22:37 -0400

    Fix half-baked solution of 2021/1

--- src/aoclj/year_2021/day_01.clj
+++ src/aoclj/year_2021/day_01.clj
@@ -3,20 +3,15 @@
 
 (def input (reader/get-input-ints 2021 1))
 
-(defn increases [size input]
+(defn increase-by [input size]
   (->> input
        (partition size 1)
        (filter #(< (first %) (last %)))
        count))
 
-(defn solve-1 [input] (increases input 2))
-
-(defn solve-2 [input] (increases input 4))
-
 (defn solve [input]
-  (let [increase-by (partial increases input)
-        solve-1 (increase-by 2)
-        solve-2 (increase-by 4)]
-    {1 (solve-1)}))
+  (let [solve-1 #(increase-by % 2)
+        solve-2 #(increase-by % 4)]
+    {1 (solve-1 input) 2 (solve-2 input)}))
 
 (time (solve input))
