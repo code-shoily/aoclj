355b009d0a9a208ffda54a0a469da924947277e0
Mafinar Khan <mafinar@gmail.com>
2025-07-24 10:44:09 -0400

    Add stub content renderer

--- src/aoclj/meta/code_org.clj
+++ src/aoclj/meta/code_org.clj
@@ -1,7 +1,8 @@
 (ns aoclj.meta.code-org
   (:require
-   [aoclj.utils :as utils]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clostache.parser :as parser]
+   [aoclj.utils :as utils]))
 
 (defn- build-file-path [prefix year day]
   (io/file
@@ -23,24 +24,43 @@
            "inputs"
            (str year "_" (utils/get-padded-day day) ".txt")))
 
+(defn- render-content-for
+  [year day template]
+  (let [padded-day (utils/get-padded-day day)]
+    (parser/render-resource
+     template
+     {:year year :day day :padded-day padded-day})))
+
+(defn render-source-content
+  [year day]
+  (render-content-for year day "templates/source.txt"))
+
+(defn render-test-content
+  [year day]
+  (render-content-for year day "templates/test.txt"))
+
+(defn fetch-input-content [_ _] "")
+
 (defn- stub-creator
-  [year day path-getter]
+  [year day path-getter renderer]
   (let [file-obj (path-getter year day)
-        file-name (.getPath file-obj)]
+        file-name (.getPath file-obj)
+        content (renderer year day)]
     (if (.exists file-obj)
       (prn (format "File %s already exists" file-name))
       (do
-        (spit file-obj "")
+        (spit file-obj content)
         (prn (format "File %s created" file-name))))))
 
 (defn create-source-stub
   [year day]
-  (stub-creator year day get-source-path))
+  (stub-creator year day get-source-path render-source-content))
 
 (defn create-test-stub
   [year day]
-  (stub-creator year day get-test-path))
+  (stub-creator year day get-test-path render-test-content))
 
 (defn create-input-stub
   [year day]
-  (stub-creator year day get-input-path))
+  (stub-creator year day get-input-path fetch-input-content))
+
