e1142fb5dfa31df3154a9f326b4cfca4c92925b0
Mafinar Khan <mafinar@gmail.com>
2025-07-25 19:40:56 -0400

    Suppress lsp on solver stats

--- src/aoclj/year_2021/solver.clj
+++ src/aoclj/year_2021/solver.clj
@@ -12,6 +12,7 @@
       ;; Add cases for days as needed
       (str "[ERROR] Not Solved: " *year* "/" day))))
 
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (defn stats
   "Returns the stats for the problem of `day` for 2021"
   [day]
