adcb60d89560574b23b221a07cb93f415832c967
Mafinar Khan <mafinar@gmail.com>
2025-08-01 00:41:20 -0400

    Solve 2016/5

--- src/aoclj/year_2016/solver.clj
+++ src/aoclj/year_2016/solver.clj
@@ -1,6 +1,7 @@
 (ns aoclj.year-2016.solver
   (:require
    [aoclj.year-2016.day-01 :as day-01]
+   [aoclj.year-2016.day-05 :as day-05]
    [aoclj.utils :as utils]))
 
 (def ^:dynamic *year* 2016)
@@ -9,6 +10,7 @@
   (let [read-input (partial utils/read-input-data *year*)]
     (case day
       1 (day-01/solve (read-input 1))
+      5 (day-05/solve (read-input 5))
       ;; Add cases for days as needed
       (str "[ERROR] Not Solved: " *year* "/" day))))
 
