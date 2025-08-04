05095e4028885ab102f9664a7717d7384d7ec04b
Mafinar Khan <mafinar@gmail.com>
2025-08-04 09:56:35 -0400

    Add community style to zprint

--- project.clj
+++ project.clj
@@ -17,9 +17,9 @@
   :profiles {:test {:dependencies [[lambdaisland/kaocha "1.91.1392"]]},
              :bench {:dependencies [[criterium "0.4.4"]]},
              :dev
-               {:plugins [[lein-cljfmt "0.9.2"] [cider/cider-nrepl "0.50.2"]],
-                :zprint {:old? false},
-                :dependencies [[nrepl "1.3.0"] [cider/cider-nrepl "0.50.2"]]}}
+             {:plugins [[lein-cljfmt "0.9.2"] [cider/cider-nrepl "0.50.2"]],
+              :zprint {:old? false, :style :community},
+              :dependencies [[nrepl "1.3.0"] [cider/cider-nrepl "0.50.2"]]}}
   :plugins [[lein-zprint "1.3.0"]]
   :aliases {"test" ["with-profile" "test" "run" "-m" "kaocha.runner"]}
   :repl-options {:init-ns aoclj.core})
