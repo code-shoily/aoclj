c1f1761416441789f8d1a9669c6c7ca44d50f5c5
Mafinar Khan <mafinar@gmail.com>
2025-08-09 22:03:19 -0400

    Fix test.txt template

--- test/aoclj/year_2015/day_09_test.clj
+++ test/aoclj/year_2015/day_09_test.clj
@@ -1,8 +1,8 @@
 (ns aoclj.year-2015.day-09-test
   (:require
-   [aoclj.year-2015.day-09 :as day-09]
-   [clojure.test :refer [is deftest]]
-   [aoclj.utils :as utils]))
+    [aoclj.year-2015.day-09 :as day-09]
+    [clojure.test :refer [is deftest]]
+    [aoclj.utils :as utils]))
 
 (def ^:dynamic *result* [117 909])
 (def input (utils/read-input-data 2015 9))
