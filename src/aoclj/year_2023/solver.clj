eaedafd10634655d30e792661d12539b302dce8a
Mafinar Khan <mafinar@gmail.com>
2025-08-17 00:17:48 -0400

    Solve 2023/04

--- src/aoclj/year_2023/solver.clj
+++ src/aoclj/year_2023/solver.clj
@@ -1,7 +1,8 @@
 (ns aoclj.year-2023.solver
   (:require [aoclj.utils :as utils]
             [aoclj.year-2023.day-01 :as day-01]
-            [aoclj.year-2023.day-02 :as day-02]))
+            [aoclj.year-2023.day-02 :as day-02]
+            [aoclj.year-2023.day-04 :as day-04]))
 
 (def ^:dynamic *year* 2023)
 
@@ -11,6 +12,7 @@
     (case day
       1 (day-01/solve (read-input day))
       2 (day-02/solve (read-input day))
+      4 (day-04/solve (read-input day))
       ;; Add cases for days as needed
       (format "[ERROR] %s/%s was not solved" *year* day))))
 
