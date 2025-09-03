587a9b019fdf3b1b625ea768a4660452179a363c
Mafinar Khan <mafinar@gmail.com>
2025-09-03 00:26:32 -0400

    Fix test value typo

--- test/aoclj/year_2020/day_10_test.clj
+++ test/aoclj/year_2020/day_10_test.clj
@@ -4,7 +4,7 @@
     [clojure.test :refer [is deftest]]
     [aoclj.utils :as utils]))
 
-(def ^:dynamic *result* [[2030 42313823813632]])
+(def ^:dynamic *result* [2030 42313823813632])
 (def input (utils/read-input-data 2020 10))
 
 (deftest year-2020-day-10-is-solved
