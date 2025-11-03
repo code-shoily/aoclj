4087b6fc34aad00d21734b1ce6a627fbb7f701c0
Mafinar Khan <mafinar@gmail.com>
2025-11-03 00:33:32 -0500

    Add test cases for transpose

--- src/aoclj/helpers/seq.clj
+++ src/aoclj/helpers/seq.clj
@@ -1,6 +1,13 @@
-(ns aoclj.helpers.seq)
+(ns aoclj.helpers.seq
+  (:require [hyperfiddle.rcf :refer [tests]]))
 
 (defn transpose
   "Transposes vector `mat`"
   [mat]
   (apply mapv vector mat))
+
+;!zprint {:format :off}
+(tests
+ (transpose [[]]) := []
+ (transpose [[1 2 3]]) := [[1] [2] [3]]
+ (transpose [[1 2 3] [4 5 6] [7 8 9] [10 11 12]]) := [[1 4 7 10] [2 5 8 11] [3 6 9 12]])
