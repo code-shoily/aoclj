3d34328fb059b2e6071f8411092d21486f7b7d94
Mafinar Khan <mafinar@gmail.com>
2025-12-01 03:10:19 -0500

    Refactor

--- src/aoclj/year_2025/day_01.clj
+++ src/aoclj/year_2025/day_01.clj
@@ -11,27 +11,26 @@
   (:require [aoclj.helpers.io :as io]
             [hyperfiddle.rcf :as rcf]))
 
-(defn parse-line
-  [[dir & val]]
-  [(if (= \L dir) - +) (parse-long (apply str val))])
+;; Quote of the day: Simulation != Arithmetic.
+
+(defn parse-line [s] [(if (= \L (nth s 0)) - +) (parse-long (subs s 1))])
 
 (defn parse [raw-input] (map parse-line (io/lines raw-input)))
 
 (defn rotate
   [[op val] cur]
-  (let [diff        (op cur (rem val 100))
-        zero-passed (quot val 100)]
-    (cond (neg-int? diff) [(+ diff 100)
-                           (if (zero? cur) zero-passed (inc zero-passed))]
-          (>= diff 100)   [(- diff 100) (inc zero-passed)]
-          (zero? diff)    [0 (inc zero-passed)]
-          :else           [diff zero-passed])))
+  (let [diff   (op cur (rem val 100))
+        zeroes (quot val 100)]
+    (cond (neg? diff)   [(+ diff 100) (if (zero? cur) zeroes (inc zeroes))]
+          (>= diff 100) [(- diff 100) (inc zeroes)]
+          (zero? diff)  [0 (inc zeroes)]
+          :else         [diff zeroes])))
 
 (defn part-1
   [input]
   (second
-   (reduce (fn [[cur pwd] [dir val]]
-             (let [[nxt _] (rotate [dir val] cur)
+   (reduce (fn [[cur pwd] move]
+             (let [[nxt _] (rotate move cur)
                    pwd*    (if (zero? nxt) (inc pwd) pwd)]
                [nxt pwd*]))
            [50 0]
@@ -41,10 +40,9 @@
   [input]
   (second
    (reduce
-    (fn [[cur pwd] [dir val]]
-      (let [[nxt zero-passed] (rotate [dir val] cur)
-            pwd* (+ pwd zero-passed)]
-        [nxt pwd*]))
+    (fn [[cur pwd] move]
+      (let [[nxt zeroes] (rotate move cur)]
+        [nxt (+ pwd zeroes)]))
     [50 0]
     input)))
 
@@ -60,8 +58,9 @@
   (time (solve raw-input))
   "</Explore>"
 )
-
 ;!zprint {:format :off}
 (rcf/enable! false)
 (rcf/tests
-  (solve (io/read-input-data 2025 1)) := [1059 6305])
+ (rotate [- 10] 0) := [90 0]
+ (rotate [- 10] 10) := [0 1]
+ (solve (io/read-input-data 2025 1)) := [1059 6305])
