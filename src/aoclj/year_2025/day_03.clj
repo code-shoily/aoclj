9909a46c23e7c56da53e51e97cc6b4e20457fd1b
Mafinar Khan <mafinar@gmail.com>
2025-12-03 01:40:21 -0500

    Format

--- src/aoclj/year_2025/day_03.clj
+++ src/aoclj/year_2025/day_03.clj
@@ -6,7 +6,7 @@
     :year       2025,
     :day        3,
     :stars      2,
-    :tags       [:sort :index]}
+    :tags       [:index :sort]}
   aoclj.year-2025.day-03
   (:require [aoclj.helpers.io :as io]
             [hyperfiddle.rcf :as rcf]))
@@ -18,38 +18,35 @@
        (io/lines raw-input)))
 
 (defn find-best-candidate
-  [indexed-coll after-idx needed]
-  (let [total-len (count indexed-coll)]
-    (->> indexed-coll
+  [icoll after-idx need]
+  (let [size (count icoll)]
+    (->> icoll
          (filter (fn [[idx _]] (> idx after-idx)))
          (sort-by second >)
          (filter (fn [[idx _]]
-                   (let [remaining-after (- total-len 1 idx)]
-                     (>= remaining-after (dec needed)))))
+                   (let [remaining-after (- size 1 idx)]
+                     (>= remaining-after (dec need)))))
          first)))
 
 (defn joltage-for
-  [n indexed-coll]
+  [n icoll]
   (loop [last-idx -1
-         needed   n
+         need     n
          acc      []]
-    (if (zero? needed)
+    (if (zero? need)
       (apply str acc)
       (let [[new-idx val]
-            (find-best-candidate indexed-coll last-idx needed)]
-        (recur new-idx (dec needed) (conj acc val))))))
+            (find-best-candidate icoll last-idx need)]
+        (recur new-idx (dec need) (conj acc val))))))
 
 (defn total-output-joltage
-  [n indexed-coll]
-  (->> indexed-coll
-       (map (partial joltage-for n))
-       (map parse-long)
+  [n icoll]
+  (->> icoll
+       (map (comp parse-long (partial joltage-for n)))
        (reduce +)))
 
 (def part-1 (partial total-output-joltage 2))
-
 (def part-2 (partial total-output-joltage 12))
-
 (def solve (io/generic-solver part-1 part-2 parse))
 
 (comment
@@ -58,10 +55,6 @@
     (io/read-input-data 2025 3))
 
   (def input (parse raw-input))
-
-  (count input)
-
-  (part-2 input)
 
   (time (solve raw-input))
   "</Explore>")
